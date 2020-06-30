5191b8b49070a6c4601aa154b887e6095a979d47
YogeshPrasanna <yogesh15031993@gmail.com>
2020-06-30 15:35:10 +0530

    add static files

---
+++ src/public/precache-manifest.f585f2b550dfc22a8a8b3151f0426c0d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f3d0d8d2cfe51aef6e439bba9ef44f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "603107f321240bc1e8ac",
+    "url": "/static/css/2.06b08db3.chunk.css"
+  },
+  {
+    "revision": "93336d0c19ebfedda008",
+    "url": "/static/css/main.12bb2aa5.chunk.css"
+  },
+  {
+    "revision": "603107f321240bc1e8ac",
+    "url": "/static/js/2.0ede793a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0ede793a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93336d0c19ebfedda008",
+    "url": "/static/js/main.18d61d5d.chunk.js"
+  },
+  {
+    "revision": "8db1c0af46e61763f595",
+    "url": "/static/js/runtime-main.fc871829.js"
+  },
+  {
+    "revision": "fe003c2d78bdd318bb1250fa0abdf77d",
+    "url": "/static/media/showcase.fe003c2d.jpg"
+  }
+]);
